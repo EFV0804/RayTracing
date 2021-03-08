2be50e5fb1196f5b9b6075553b5c89ff32702450
unknown <evidal@artfx.fr>
2021-03-08 12:40:27 +0100

    ray vec

--- main.cpp
+++ main.cpp
@@ -1,59 +1,50 @@
 #include <iostream>
 #include <fstream>
-#include <limits>
-
-#include "HittableList.h"
+#include "Ray.h"
 #include "Sphere.h"
-
-float MAX_FLOAT = std::numeric_limits<float>::max();
-
-Vec3 color(const Ray &r, Hittable *world) {
-    HitRecord rec;
-    if (world->hit(r, 0.0, MAX_FLOAT, rec)) {
-        return 0.5 * Vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
-    } else {
-        Vec3 unitDirection = unitVector(r.direction());
-        float t = 0.5 * (unitDirection.y() + 1.0);
-        return (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0);
-    }
-}
+/*bool IsSphereHit(const Vec3& center, float radius, const Ray& r)
+{    
+	Vec3 oc = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0 * dot(oc, r.direction());float c = dot(oc, oc) - radius*radius;
+	float discriminant = b*b - 4*a*c;
+	return (discriminant > 0);
+}*/
+Vec3 color(const Ray& r){    
+	/*if (IsSphereHit(Vec3(0,0,-1), 0.5, r))
+	{
+		return Vec3(1, 0, 0);
+	}*/
+	Vec3 unitDirection = unitVector(r.direction());
+	float t = 0.5*(unitDirection.y() + 1.0);
+	return (1.0-t)*Vec3(1.0, 1.0, 1.0) + t*Vec3(0.5, 0.7, 1.0);}
 
 int main() {
-    std::ofstream output;
+	std::ofstream output;
     output.open("output.ppm");
+	//Sphere sphere = Sphere(Vec3(0, 0, -1), 0.5);
 
-    int nx = 200;
-    int ny = 100;
-    output << "P3\n" << nx << " " << ny << "\n255\n";
-
-    Vec3 lowerLeftCorner(-2.0, -1.0, -1.0);
-    Vec3 horizontal(4.0, 0.0, 0.0);
-    Vec3 vertical(0.0, 2.0, 0.0);
-    Vec3 origin(0.0, 0.0, 0.0);
-
-    Hittable *list[2];
-    list[0] = new Sphere(Vec3(0, 0, -1), 0.5);
-    list[1] = new Sphere(Vec3(0, -100.5, -1), 100);
-    Hittable *world = new HittableList(list, 2);
-
-    for (int j = ny - 1; j >= 0; j--) {
-        for (int i = 0; i < nx; i++) {
-
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
-            Ray r(origin, lowerLeftCorner + u * horizontal + v * vertical);
-
-            Vec3 p = r.pointAtParameter(2.0);
-            Vec3 col = color(r, world);
-
-            int ir = int(255.99 * col[0]);
-            int ig = int(255.99 * col[1]);
-            int ib = int(255.99 * col[2]);
-
-            output << ir << " " << ig << " " << ib << "\n";
-        }
-    }
-
-    output.close();
-    return 0;
+	int nx = 200;
+	int ny = 100;
+	output << "P3\n" << nx << "" << ny << "\n255\n";
+	Vec3 lower_left_corner(-2.0, -1.0, -1.0);
+	Vec3 horizontal(4.0, 0.0, 0.0);
+	Vec3 vertical(0.0, 2.0, 0.0);
+	Vec3 origin(0.0, 0.0, 0.0);
+	for(int j = ny-1; j>=0; j--)
+	{
+		for(int i = 0; i < nx; i++)
+		{
+			float u = float(i) / float (nx);
+			float v = float(j) / float (ny);
+			Ray r (origin, lower_left_corner + u*horizontal + v*vertical);
+			Vec3 col = color(r);
+			int ir = int (255.99*col[0]);
+			int ig = int (255.99*col[1]);
+			int ib = int (255.99*col[2]);
+			output << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+	output.close();
+	return 0;
 }
